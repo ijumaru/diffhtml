552aecf4e9c63e404341fa3a9f27ee1e0dc4f584
nr <cocoapebles@gmail.com>
2013-11-13 00:38:57 +0900

    only changed method

--- ClassParser.php
+++ ClassParser.php
@@ -10,6 +10,8 @@ class ClassParser {
 		$fo = new SplFileObject($this->fileName);
 		$methods = array();
 		$datas = array();
+		$indent = 0;
+		$isChanged = false;
 		while (!$fo->eof()) {
 			$line = $fo->fgets();
 			$patternClass = '/^[ -+]\t*( *[a-zA-Z0-9]*)*?class ([a-zA-Z0-9]+) .*?{/i';
@@ -23,7 +25,23 @@ class ClassParser {
 			}
 			$patternMethod = '/^[ -+]\t*( *[a-zA-Z0-9]*)*? ([a-zA-Z0-9]*?) *?\(.*?\).*?{/i';
 			if (preg_match($patternMethod, $line, $matches) === 1 && !empty($matches[2])) {
-				$methods[] = $matches[2];
+				$method = $matches[2];
+				$indent = 1;
+			}
+			if (strpos($line, "{") !== false && $indent > 0) { // strpos() >= 0は正しく動作しない
+				$indent++;
+			}
+			if (strpos($line, "}") !== false && $indent > 0) {
+				$indent--;
+				if ($indent == 1 && $isChanged === true) {
+					$methods[] = $method;
+					$isChanged = false;
+					$indent = 0;
+					$method = "";
+				}
+			}
+			if ($indent > 0 && (strpos($line, "+") === 0 || strpos($line, "-") === 0)) {
+				$isChanged = true;
 			}
 		}
 		if (count($methods) > 0) {
